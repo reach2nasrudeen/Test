41a974eb148d3a7e24f740ce5532c38dc19d421f
Nasrudeen <reach2nasrudeen@gmail.com>
2024-07-29 16:12:57 +0530

    data binding added

--- app/src/main/java/com/interview/test/MainActivity.kt
+++ app/src/main/java/com/interview/test/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.interview.test
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.interview.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNavigationView.apply {
+            background = null
+            menu.getItem(2).isEnabled = false
+        }
     }
 }
