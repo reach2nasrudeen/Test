83fa416483d5ec39e9a1d2408a3300f6abe2d1df
Nasrudeen <reach2nasrudeen@gmail.com>
2024-07-29 17:25:50 +0530

    navigation screens added

---
+++ app/src/main/java/com/interview/test/CardListingFragment.kt
@@ -0,0 +1,29 @@
+package com.interview.test
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CardListingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CardListingFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_card_listing, container, false)
+    }
+
+
+}
--- app/src/main/java/com/interview/test/MainActivity.kt
+++ app/src/main/java/com/interview/test/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.interview.test
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.interview.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+
+    private var previousSelectedItem: Int = R.id.navigation_dashboard
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -14,6 +23,45 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView.apply {
             background = null
             menu.getItem(2).isEnabled = false
+        }
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Test Action", Toast.LENGTH_SHORT).show()
+        }
+        (binding.navHostFragment.getFragment() as? NavHostFragment)?.navController?.let {
+            navController = it
+            binding.bottomNavigationView.setupWithNavController(navController)
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            val currentSelectedItem = item.itemId
+
+//            if (this.previousSelectedItem != currentSelectedItem) {
+//                navController.popBackStack(previousSelectedItem, false)
+//            }
+
+//
+//            // Clear the back stack by popping to the selected destination
+//            navController.popBackStack(item.itemId, false)
+
+            when (item.itemId) {
+                R.id.navigation_action -> {
+                    Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navigation_statistics -> {
+                    Toast.makeText(this, "Test Stats", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navigation_profile -> {
+                    Toast.makeText(this, "Test Profile", Toast.LENGTH_SHORT).show()
+                }
+
+                else -> {
+                    NavigationUI.onNavDestinationSelected(item, navController)
+                }
+            }
+            this.previousSelectedItem = currentSelectedItem
+
+            true
         }
     }
 }
