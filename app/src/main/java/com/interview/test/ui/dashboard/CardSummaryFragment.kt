c8f170ce636529d8254eeabb7b6934d217876ad2
Nasrudeen <reach2nasrudeen@gmail.com>
2024-08-01 03:43:17 +0530

    chart styles updated

--- app/src/main/java/com/interview/test/ui/dashboard/CardSummaryFragment.kt
+++ app/src/main/java/com/interview/test/ui/dashboard/CardSummaryFragment.kt
@@ -8,36 +8,28 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.interview.test.R
 import com.interview.test.adapter.TransactionsAdapter
-import com.interview.test.base.Constants
 import com.interview.test.databinding.FragmentCardSummaryBinding
-import com.interview.test.model.CardResponse
-import com.interview.test.model.Transaction
-import com.interview.test.utils.getColorRes
-import com.interview.test.utils.getObjectFromJson
-import com.interview.test.utils.toModelString
+import com.interview.test.model.GraphType
 import com.interview.test.viewmodel.CardSummaryViewModel
 import com.patrykandpatrick.vico.core.cartesian.axis.AxisPosition
-import com.patrykandpatrick.vico.core.cartesian.axis.BaseAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
 import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
 import com.patrykandpatrick.vico.core.cartesian.marker.DefaultCartesianMarker
+import com.patrykandpatrick.vico.core.common.component.LineComponent
 import com.patrykandpatrick.vico.core.common.component.TextComponent
 import com.patrykandpatrick.vico.core.common.data.ExtraStore
+import com.patrykandpatrick.vico.core.common.shape.Shape
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.util.Date
-import java.util.Locale
-import kotlin.math.abs
-import kotlin.random.Random
 
 
 /**
@@ -65,17 +57,6 @@ class CardSummaryFragment : Fragment() {
         return binding.root
     }
 
-    fun parseDate(input: String): Date {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-//        dateFormat.timeZone = TimeZone.getTimeZone(ZoneId.systemDefault())
-        return dateFormat.parse(input)!!
-    }
-
-    fun toText(input: Date): String {
-        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-        return dateFormat.format(input)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -89,183 +70,43 @@ class CardSummaryFragment : Fragment() {
         }
         setupTransactions()
         setupObserver()
+        setupTabs()
 
-//        viewModel.getCardSummary()
-
-        lifecycleScope.launch {
-            requireContext().getObjectFromJson<CardResponse>("card_summary.json").let {
-                viewModel.updateCardSummary(it)
-            }
-        }
+        viewModel.getCardSummary()
 
         binding.btnTryNow.setOnClickListener {
             viewModel.getCardSummary()
         }
 
-        viewModel.transactions.observe(viewLifecycleOwner) { transactions ->
+        viewModel.transactions.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
 
-
-            transactions.filter { it.status == "success" && it.type == "debit" }
-                .groupBy { LocalDate.parse(it.date.orEmpty(), DateTimeFormatter.ofPattern(Constants.TF_DEFAULT_1)) }
-//                .mapKeys { entry -> LocalDate.parse(entry.key) }
-                .mapValues { entry -> entry.value.sumOf { abs(it.amount ?: 0.0) } }
-                .also {
-                    if (it.keys.isNotEmpty()) {
-                        setupChart2(it)
-                    }
-                }
-                .forEach { transaction ->
-                    Timber.e("date---->${transaction.key}-------amount---->${transaction.value}")
-
-//                val date = parseDate(transaction.date.orEmpty())
-//                val dateOnly = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
-//                dailyTotals[dateOnly] = (dailyTotals.getOrDefault(dateOnly, 0f) + (transaction.amount ?: 0.0)).toFloat()
-                }
+                setupDayChart(viewModel.getTransactionsForDay())
+                setupMonthChart(viewModel.getTransactionsForMonth())
+                setupYearChart(viewModel.getTransactionsForYear())
+            }
         }
 
-//        setupChart(viewModel.transactions.value.orEmpty())
-        /*binding.chartView.apply {
-//            chart?.persistentMarkers = { marker at PERSISTENT_MARKER_X }
-//            PersistentMarkerScope
-//            val horizontalLineYKey = ExtraStore.Key<Float>()
-            val data = listOf("2022-07-01" to 2f, "2022-07-02" to 6f, "2022-07-04" to 4f).associate { (dateString, yValue) ->
-                LocalDate.parse(dateString) to yValue
-            }
-            val xValuesToDates = data.keys.associateBy { it.toEpochDay().toFloat() }
-//            val chartEntryModel = entryModelOf(xValuesToDates.keys.zip(data.values, ::entryOf))
-//            val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMM")
-//            val horizontalAxisValueFormatter = AxisValueFormatter<AxisPosition.Horizontal> { value, _ ->
-//                (xValuesToDates[value] ?: LocalDate.ofEpochDay(value.toLong())).format(dateTimeFormatter)
-//            }
+    }
 
-            chart?.bottomAxis  = HorizontalAxis.bottom(
+    private fun setupDayChart(data: Map<LocalDate, Double>) {
+        if (data.isEmpty()) return
+        val dayChartModelProducer = CartesianChartModelProducer()
+        binding.chartViewDay.apply {
+            this.modelProducer = dayChartModelProducer
+            (chart?.bottomAxis as? HorizontalAxis)?.label = TextComponent(color = Color.BLACK)
+            (chart?.startAxis as? VerticalAxis)?.label = TextComponent(color = Color.BLACK)
+            chart?.marker = DefaultCartesianMarker(
                 label = TextComponent(color = Color.BLACK),
-            )
-            chart?.startAxis  = VerticalAxis.start(
-                label = TextComponent(color = Color.BLUE),
-                titleComponent = TextComponent(color = Color.RED),
-            )
-//            ( chart?.bottomAxis as? HorizontalAxis)?.titleComponent = TextComponent(color = Color.RED)
-//            startAxis
-//            this.setModel(chartEntryModel)
-
-            val modelProducer = CartesianChartModelProducer()
-
-//            val data =
-//                mapOf(
-//                    LocalDate.parse("2022-07-01") to 2f,
-//                    LocalDate.parse("2022-07-02") to 6f,
-//                    LocalDate.parse("2022-07-04") to 4f,
-//                )
-            val xToDateMapKey = ExtraStore.Key<Map<Float, LocalDate>>()
-//            chart?.bottomAxis = HorizontalAxis.buil {
-//                valueFormatter = { x, _ -> "Label $x" }
-//                // Other configurations
-//            }
-            val xToDates = data.keys.associateBy { it.toEpochDay().toFloat() }
-//            modelProducer.runTransaction {
-//                lineSeries { series(xToDates.keys, data.values) }
-//                extras { it[xToDateMapKey] = xToDates }
-//            }
-
-            val dateTimeFormatter = DateTimeFormatter.ofPattern("d MMM")
-//            CartesianValueFormatter { x, chartValues, _ ->
-//                (chartValues.model.extraStore[xToDateMapKey][x] ?: LocalDate.ofEpochDay(x.toLong()))
-//                    .format(dateTimeFormatter)
-//            }
-
-            val marker = DefaultCartesianMarker(
-                label = TextComponent()
+                labelPosition = DefaultCartesianMarker.LabelPosition.AroundPoint,
+                guideline = LineComponent(
+                    color = Color.BLACK,
+                    shape = Shape.Rectangle,
+                    strokeColor = Color.BLACK,
+                    strokeThicknessDp = 5f
+                )
             )
 
-//            (chart?.bottomAxis as BaseAxis).guideline = null
-//            (chart?.bottomAxis as BaseAxis).titleComponent = TextComponent(
-//                color = requireContext().getColorRes(R.color.red)
-//            )
-            this.modelProducer = modelProducer
-            chart?.persistentMarkers = { marker at PERSISTENT_MARKER_X }
-            lifecycleScope.launch {
-                modelProducer.runTransaction {
-//                    lineSeries { series(xValue, xValue.map { Random.nextFloat() * 15 }) }
-                    lineSeries { series(xValuesToDates.keys, data.values) }
-                    extras { it[xToDateMapKey] = xToDates }
-                }
-
-            }
-        }*/
-    }
-//    fun parseDate(dateStr: String): Date {
-//        val dateFormat = java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
-//        dateFormat.timeZone = TimeZone.getTimeZone(ZoneId.systemDefault())
-//        return dateFormat.parse(dateStr)!!
-//    }
-
-    private fun setupChart(transactions: List<Transaction>) {
-//        transactions.filter { it.status == "success" && it.type == "debit"}.sortedByDescending { parseDate(it.date.orEmpty()) }.forEach { transaction ->
-//            val date = parseDate(transaction.date.orEmpty())
-//            val dateOnly = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
-//            dailyTotals[dateOnly] = (dailyTotals.getOrDefault(dateOnly, 0f) + (transaction.amount ?: 0.0)).toFloat()
-//        }
-
-        binding.chartView.apply {
-
-            (chart?.bottomAxis as? HorizontalAxis)?.label = TextComponent(
-                color = Color.BLACK
-            )
-            (chart?.startAxis as? VerticalAxis)?.label = TextComponent(
-                color = Color.BLACK
-            )
-
-            val modelProducer = CartesianChartModelProducer()
-
-
-//            chart?.bottomAxis = HorizontalAxis.bottom(
-//                label = TextComponent(color = Color.BLACK),
-//            )
-//            chart?.startAxis = VerticalAxis.start(
-//                label = TextComponent(color = Color.BLACK),
-//            )
-
-//            val marker = DefaultCartesianMarker()
-
-            (chart?.bottomAxis as BaseAxis).titleComponent = TextComponent(
-                color = Color.RED
-            )
-            this.modelProducer = modelProducer
-//            chart?.persistentMarkers = { marker at PERSISTENT_MARKER_X }
-            lifecycleScope.launch {
-                modelProducer.runTransaction {
-                    lineSeries { series(xValue, xValue.map { Random.nextFloat() * 15 }) }
-//                    lineSeries { series(xValuesToDates.keys, data.values) }
-//                    extras { it[xToDateMapKey] = xToDates }
-                }
-
-
-            }
-        }
-    }
-
-    private fun setupChart2(data: Map<LocalDate, Double>) {
-        Timber.e("setupChart2--came here")
-        val xValues = data.keys.toList()
-        val yValues = data.values.toList()
-//        transactions.filter { it.status == "success" && it.type == "debit"}.sortedByDescending { parseDate(it.date.orEmpty()) }.forEach { transaction ->
-//            val date = parseDate(transaction.date.orEmpty())
-//            val dateOnly = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
-//            dailyTotals[dateOnly] = (dailyTotals.getOrDefault(dateOnly, 0f) + (transaction.amount ?: 0.0)).toFloat()
-//        }
-        val modelProducer = CartesianChartModelProducer()
-        binding.chartView.apply {
-            this.modelProducer = modelProducer
-            (chart?.bottomAxis as? HorizontalAxis)?.label = TextComponent(
-                color = Color.BLACK
-            )
-            (chart?.startAxis as? VerticalAxis)?.label = TextComponent(
-                color = Color.BLACK
-            )
-//            (chart?.bottomAxis as HorizontalAxis<AxisPosition.Horizontal.Bottom>).valueFormatter = CartesianValueFormatter { x, _, _ ->
-//                xValues[x.toInt()]
-//                }
             val xToDateMapKey = ExtraStore.Key<Map<Float, LocalDate>>()
             val xToDates = data.keys.associateBy { it.toEpochDay().toFloat() }
             val dateTimeFormatter = DateTimeFormatter.ofPattern("d MMM")
@@ -277,45 +118,107 @@ class CardSummaryFragment : Fragment() {
                         .format(dateTimeFormatter)
                 }
             lifecycleScope.launch {
-                modelProducer.runTransaction {
+                dayChartModelProducer.runTransaction {
                     lineSeries { series(xToDates.keys, data.values) }
                     extras { it[xToDateMapKey] = xToDates }
                 }
-
             }
-
-
-//            val labelListKey = ExtraStore.Key<List<String>>()
-//
-//            (chart?.bottomAxis as HorizontalAxis<AxisPosition.Horizontal.Bottom>).valueFormatter =
-//                CartesianValueFormatter { x, chartValues, _ ->
-//                    chartValues.model.extraStore[labelListKey][x.toInt()]
-//                }
-
-
-
-//            chart?.persistentMarkers = { marker at PERSISTENT_MARKER_X }
-//            lifecycleScope.launch {
-//                modelProducer.runTransaction {
-//                    lineSeries { series(yValues) }
-////                    lineSeries { series(xValuesToDates.keys, data.values) }
-//                    extras { it[labelListKey] = xValues }
-//
-//                }
-//            }
         }
     }
 
-    companion object {
-        private val xValue = (1..50).toList()
+    private fun setupMonthChart(data: Map<LocalDate, Double>) {
+        if (data.isEmpty()) return
+        val monthChartModelProducer = CartesianChartModelProducer()
+        binding.chartViewMonth.apply {
+            this.modelProducer = monthChartModelProducer
+            (chart?.bottomAxis as? HorizontalAxis)?.label = TextComponent(color = Color.BLACK)
+            (chart?.startAxis as? VerticalAxis)?.label = TextComponent(color = Color.BLACK)
 
-        private const val PERSISTENT_MARKER_X = 7f
+            chart?.marker = DefaultCartesianMarker(
+                label = TextComponent(color = Color.BLACK),
+                labelPosition = DefaultCartesianMarker.LabelPosition.AroundPoint,
+                guideline = LineComponent(
+                    color = Color.BLACK,
+                    shape = Shape.Pill,
+                    strokeColor = Color.BLACK,
+                    strokeThicknessDp = 5f
+                )
+            )
 
+            val xToDateMapKey = ExtraStore.Key<Map<Float, LocalDate>>()
+            val xToDates = data.keys.associateBy { it.toEpochDay().toFloat() }
+            val dateTimeFormatter = DateTimeFormatter.ofPattern("MMM")
+
+            (chart?.bottomAxis as HorizontalAxis<AxisPosition.Horizontal.Bottom>).valueFormatter =
+                CartesianValueFormatter { x, chartValues, _ ->
+                    (chartValues.model.extraStore[xToDateMapKey][x.toFloat()]
+                        ?: LocalDate.ofEpochDay(x.toLong()))
+                        .format(dateTimeFormatter)
+                }
+            lifecycleScope.launch {
+                monthChartModelProducer.runTransaction {
+                    lineSeries { series(xToDates.keys, data.values) }
+                    extras { it[xToDateMapKey] = xToDates }
+                }
+            }
+        }
+    }
+
+    private fun setupYearChart(data: Map<Int, Double>) {
+        if (data.isEmpty()) return
+        val yearChartModelProducer = CartesianChartModelProducer()
+        binding.chartViewYear.apply {
+            this.modelProducer = yearChartModelProducer
+            (chart?.bottomAxis as? HorizontalAxis)?.label = TextComponent(color = Color.BLACK)
+            (chart?.startAxis as? VerticalAxis)?.label = TextComponent(color = Color.BLACK)
+            chart?.marker = DefaultCartesianMarker(
+                label = TextComponent(color = Color.BLACK),
+                labelPosition = DefaultCartesianMarker.LabelPosition.AroundPoint,
+                guideline = LineComponent(
+                    color = Color.BLACK,
+                    shape = Shape.rounded(15),
+                    strokeColor = Color.BLACK,
+                    strokeThicknessDp = 5f
+                )
+            )
+
+            lifecycleScope.launch {
+                yearChartModelProducer.runTransaction {
+                    lineSeries { series(data.keys, data.values) }
+                }
+            }
+        }
     }
 
     private fun setupTransactions() {
         transactionsAdapter = TransactionsAdapter()
         binding.rvTransactions.adapter = transactionsAdapter
+    }
+
+    private fun setupTabs() {
+        binding.tabLayout.getTabAt(1)?.select()
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                when (tab?.text) {
+                    getString(R.string.text_day) -> GraphType.DAY
+                    getString(R.string.text_month) -> GraphType.MONTH
+                    getString(R.string.text_yearly) -> GraphType.YEARLY
+                    else -> null
+                }?.let {
+                    viewModel.updateSelectedGraphType(it)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // No-Op
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // No-Op
+            }
+
+        })
     }
 
     private fun setupObserver() {
--- app/src/main/java/com/interview/test/viewmodel/CardSummaryViewModel.kt
+++ app/src/main/java/com/interview/test/viewmodel/CardSummaryViewModel.kt
@@ -3,7 +3,9 @@ package com.interview.test.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
+import com.interview.test.base.Constants
 import com.interview.test.model.CardResponse
 import com.interview.test.model.GraphType
 import com.interview.test.model.SummaryUiState
@@ -13,23 +15,28 @@ import com.interview.test.network.asResult
 import com.interview.test.repository.CardsRepository
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import kotlin.math.abs
 
 class CardSummaryViewModel(private val cardsRepository: CardsRepository) : ViewModel() {
-
 
     private val _balance = MutableLiveData(0.0)
     val balance: LiveData<Double> get() = _balance
 
     private val _graphType = MutableLiveData(GraphType.MONTH)
-    val graphType: LiveData<GraphType> get() = _graphType
+    private val graphType: LiveData<GraphType> get() = _graphType
 
     private val _summaryUiState = MutableLiveData(SummaryUiState())
     val summaryUiState: LiveData<SummaryUiState> get() = _summaryUiState
 
-
     private val _transactions = MutableLiveData<List<Transaction>>(emptyList())
     val transactions: LiveData<List<Transaction>> get() = _transactions
 
+    val isDayGraphSelected: LiveData<Boolean> get() = graphType.map { it == GraphType.DAY }
+    val isMonthGraphSelected: LiveData<Boolean> get() = graphType.map { it == GraphType.MONTH }
+    val isYearGraphSelected: LiveData<Boolean> get() = graphType.map { it == GraphType.YEARLY }
 
     fun getCardSummary() {
         viewModelScope.launch {
@@ -59,5 +66,59 @@ class CardSummaryViewModel(private val cardsRepository: CardsRepository) : ViewM
         _summaryUiState.postValue(SummaryUiState(success = true))
         _transactions.postValue(cardResponse.transactions.orEmpty())
         _balance.postValue(cardResponse.cardSummary?.currentBalance ?: 0.0)
+    }
+
+    fun updateSelectedGraphType(type: GraphType) {
+        _graphType.postValue(type)
+    }
+
+    fun getTransactionsForDay(): Map<LocalDate, Double> {
+        return transactions.value?.filter { transaction ->
+            transaction.status == "success" && transaction.type == "debit"
+        }?.groupBy {
+            LocalDate.parse(
+                it.date.orEmpty(),
+                DateTimeFormatter.ofPattern(Constants.TF_DEFAULT_1)
+            )
+        }?.mapValues { entry -> entry.value.sumOf { abs(it.amount ?: 0.0) } }.orEmpty()
+    }
+
+    fun getTransactionsForMonth(): Map<LocalDate, Double> {
+        return transactions.value?.filter { transaction ->
+            transaction.status == "success" && transaction.type == "debit"
+        }?.groupBy {
+            YearMonth.from(
+                LocalDate.parse(
+                    it.date.orEmpty(),
+                    DateTimeFormatter.ofPattern(Constants.TF_DEFAULT_1)
+                )
+            ).atEndOfMonth()
+        }?.mapValues { entry -> entry.value.sumOf { abs(it.amount ?: 0.0) } }.orEmpty()
+    }
+
+    fun getTransactionsForYear(): Map<Int, Double> {
+        val startYear = LocalDate.now().year - 1
+        val endYear = LocalDate.now().year + 1
+
+
+        val yearlyData = mutableMapOf<Int, Double>().apply {
+            for (year in startYear..endYear) {
+                this[year] = 0.0
+            }
+        }
+
+        transactions.value?.filter { transaction ->
+            transaction.status == "success" && transaction.type == "debit"
+        }?.forEach { transaction ->
+            val transactionYear = LocalDate.parse(
+                transaction.date.orEmpty(),
+                DateTimeFormatter.ofPattern(Constants.TF_DEFAULT_1)
+            ).year
+
+            yearlyData[transactionYear] =
+                yearlyData[transactionYear]?.plus(abs(transaction.amount ?: 0.0)) ?: 0.0
+        }
+
+        return yearlyData
     }
 }
